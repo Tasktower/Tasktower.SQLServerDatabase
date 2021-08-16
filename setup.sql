2dbef7f6251729529ce4d1ab2f544b3f1600b692
Oren Ben-Meir <orenbm11@gmail.com>
2021-08-15 20:51:21 -0400

    added user db

--- setup.sql
+++ setup.sql
@@ -1,5 +1,10 @@
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'project')
-BEGIN
-  CREATE DATABASE project;
-END;
-GO
+    BEGIN
+      CREATE DATABASE [project];
+    END;
+
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'user')
+    BEGIN
+        CREATE DATABASE [user];
+    END;
+GO;
